f546c3ad1a70ab90c1e3807e24862bfad2bf7b36
horo2016 <selectld@163.com>
2022-06-15 10:00:39 +0800

    Add files via upload

---
+++ inc/mqtt_main.h
@@ -0,0 +1,20 @@
+#ifndef MQTT_MAIN_H
+#define  MQTT_MAIN_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void *Mqtt_ClentTask(void *argv);
+extern void *Mqtt_PublishTask(void *argv);
+
+extern int get_value_from_cmdline( char* buf, char *key, char *value);
+
+extern void User_MsgContl(char * msg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
